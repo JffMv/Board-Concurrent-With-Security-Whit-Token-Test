3660854b6f33645c0e8db0c756cde230005ef31c
Jefferson Mesa <mesajefer2@gmail.com>
2024-07-11 11:01:18 -0500

    add security to project

---
+++ src/main/java/co/edu/ing/escuela/appWebSocket/MvcConfig.java
@@ -0,0 +1,19 @@
+package co.edu.ing.escuela.appWebSocket;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/board").setViewName("index");
+        registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/board").setViewName("index");
+    }
+
+}
